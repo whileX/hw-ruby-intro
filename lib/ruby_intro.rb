bb82a5f1d7ba3626fcbaa6eb833b6570f36b0ced
Yixiao <yixvon@gmail.com>
2018-02-12 02:35:05 +0000

    wrote hw1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,95 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  s = 0
+  if (arr.empty?)
+      return s
+  else
+      arr.each do |i|
+          s += i
+      end
+      return s
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum = 0
+  if (arr.empty?)
+      return sum
+  elsif arr.size == 1
+      return arr[0]
+  else
+      sortArray = arr.sort.reverse
+      sum = sortArray[0] + sortArray[1]
+      return sum
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  
+  if arr.size == 1
+    return false
+  end
+  
+  arr.each_with_index do |o, i|
+      arr[i+1..-1].each do |j|
+          if arr[i] + j == n
+              return true
+          end
+      end
+  end
+  return false
+  
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return 'Hello, ' + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  cons = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+  if s.empty?
+      return false
+  end
+  
+  if s[0][/[a-zA-Z]+/]  == s[0]
+    return (cons.include? s[0]).!
+  else
+    return false
+  end
+    
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  begin
+    return Integer(s) % 4 == 0
+  rescue
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  def initialize(i,p)
+      
+      if i.length != 0 and p > 0
+          @isbn = i
+          @price = p
+      else
+          raise ArgumentError
+      end
+  end
+  
+  def price_as_string
+      puts '$' + sprintf('%.2f', @price)
+      return '$' + sprintf('%.2f', @price)
+  end
+  
+  attr_accessor :isbn
+  attr_accessor :price
+  
 end
